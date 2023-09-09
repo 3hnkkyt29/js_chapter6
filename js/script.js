a4ed1cf8dd0303982b5a09725877a865605fc9fd
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-09-09 12:54:12 +0000

    finish 6-6

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ Web Animations APIの基本の書き方
 const heading = document.querySelector('#heading');
 const keyframes = {
   opacity: [0,1], //開始時は0＝透明、終了時は1＝不透明
+  translate: ['0 50px', 0],
 }
 heading.animate(keyframes, 2000);
 // 1秒＝1000　すなわち上記は2秒かけて再生するように指示している

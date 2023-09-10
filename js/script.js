51e7539b3770e16e744207f2ff65c43c5affe750
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-09-10 10:45:51 +0000

    finish 6-7

--- js/script.js
+++ js/script.js
@@ -18,8 +18,14 @@ const heading = document.querySelector('#heading');
 const keyframes = {
   opacity: [0,1], //開始時は0＝透明、終了時は1＝不透明
   translate: ['0 50px', 0],
-}
-heading.animate(keyframes, 2000);
+};
+// 6-7
+const options = {
+  duration: 2000,
+  easing: 'ease',
+};
+// heading.animate(keyframes, 2000); をoptionsを呼び出す以下のように変更
+heading.animate(keyframes, options);
 // 1秒＝1000　すなわち上記は2秒かけて再生するように指示している
 
 // 6-6
@@ -45,4 +51,21 @@ heading.animate(keyframes, 2000);
 
 上記①②のは同じ意味
 井戸に動く内容が多い場合は②が適している
+*/
+
+// 6-7
+/*
+動かす時間や、タイミング、繰り返しの記述方法の基本
+「動かす要素.animate(動かす内容, 動きの詳細);」
+
+「
+const keyframes = {
+  キー（プロパティ名）:['開始の値', '終了の値'」,
+};
+cont options = {
+  キー（プロパティ名）: '値',
+};
+動かす要素.animate(keyframes, options);
+」
+（定数名はなんでも良いよ）
 */

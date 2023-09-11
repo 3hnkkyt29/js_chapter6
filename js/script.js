5ee11dbef24f4c3f7ed651cf51c8829f2f74f212
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-09-11 11:14:09 +0000

    middle of 6-8 * change colors of letter

--- js/script.js
+++ js/script.js
@@ -16,13 +16,18 @@ Web Animations APIの基本の書き方
 */
 const heading = document.querySelector('#heading');
 const keyframes = {
-  opacity: [0,1], //開始時は0＝透明、終了時は1＝不透明
-  translate: ['0 50px', 0],
+  // opacity: [0,1], //開始時は0＝透明、終了時は1＝不透明
+  // 6-8
+  // rotate: ['x 360deg, 0'],
+  // translate: ['0 50px', 0],
+  color: ['#f66', '#fc2', '#0c6', '#0bd']
 };
 // 6-7
 const options = {
-  duration: 2000,
-  easing: 'ease',
+  duration: 8000,
+  direction: 'alternate', //終了地点に来たら開始地点に戻す指定
+  iterations: Infinity, //ずっと繰り返す
+  //easing: 'ease',　アニメーションが変化する速度：開始時と終了時はゆっくり
 };
 // heading.animate(keyframes, 2000); をoptionsを呼び出す以下のように変更
 heading.animate(keyframes, options);

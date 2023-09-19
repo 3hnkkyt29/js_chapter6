84b46537cea09154123c8e68a11955b02c516ffc
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-09-19 13:59:17 +0000

    6-10 * made explanation

--- js/script2.js
+++ js/script2.js
@@ -1,14 +1,15 @@
 const items = document.querySelectorAll('.img-item');
 // // cssファイルと同様「.img-item」にする必要あり、「.」忘れない
+// 取得する要素はIDではなくクラスなのでカッコ内は「.img-item」
 
 for (let i = 0; i < items.length; i++) {
   const keyframes = {
-    opacity:[0,1]
+    opacity:[0,1] //透明から不透明に
   };
   const options = {
-    duration: 600,
-    delay: i * 300,
-    fill: 'forwards',
+    duration: 600, //再生時間600ミリ秒
+    // delay: i * 300,
+    fill: 'forwards', //再生後不透明を維持
   };
   items[i].animate(keyframes, options);
 }

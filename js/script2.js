38e8507f9da6347bc5248eb602018e10af2c235e
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-09-22 13:07:39 +0000

    middle of 6-12

--- js/script2.js
+++ js/script2.js
@@ -4,7 +4,9 @@ const items = document.querySelectorAll('.img-item');
 
 for (let i = 0; i < items.length; i++) {
   const keyframes = {
-    opacity:[0,1] //透明から不透明に
+    opacity:[0,1], //透明から不透明に
+    //rotate:['x 90deg', 0], //回転しながら画像が現れる
+    translate:['0 50px', 0], //下から浮き上がってくるように画像が現れる
   };
   const options = {
     duration: 600, //再生時間600ミリ秒

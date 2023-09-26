fe788e6f76a7237a807a37483be4eee2cd839da4
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-09-26 13:01:10 +0000

    finish 6-14

---
+++ js/script3.js
@@ -0,0 +1,15 @@
+//監視対象が範囲内に現れたら実行する動作
+const showKirin = () => {
+  console.log('キリンさんです');
+};
+//監視ロボットの設定
+const kirinObserver = new IntersectionObserver(showKirin);
+//#Kirinを監視するよう指示
+kirinObserver.observe(document.querySelector('#kirin'));
+
+/*
+1〜8行目をまとめると
+監視ロボット「kirinObserver」に
+「「#kirin」を監視してね、エリア内に入ったらコンソールに「キリンさんです」と出力してね」
+という指示を送った
+*/
